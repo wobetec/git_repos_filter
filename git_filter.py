66fc44a339b5044c27d521eb073560de5775be6c
wobetec <esdras2821@gmail.com>
2022-05-11 11:37:29 -0300

    Atualizado para receber exclusão

--- git_filter.py
+++ git_filter.py
@@ -10,29 +10,48 @@ ACCESS_TOKEN = ""
 with open("C:\Code\git_token.txt", "r") as f:
     ACCESS_TOKEN = f.read()
 
+
 def token_auth(request):
-    request.headers["User-Agent"] = "Minicurso" # Necessário
+    request.headers["User-Agent"] = "wobetec" # Necessário
     request.headers["Authorization"] = "token {}".format(ACCESS_TOKEN)
     return request
 
-class Query_string():
 
-    def __init__(self, keywords = [], parameters = {}, pag_count = 100, all_pages = False, more = {}):
+class Query_string():
+    """
+    Objeto construido a partir de um dicionário e que ao usar __str__() retorna a string da query para realizar o request
+
+    Argumentos aceitos no dicionário:
+        --> keywords = dicionário contendo duas listas de termos "include" e "exclude"
+        --> parameters = dicionário contendo dois dicionários com os parametros a serem adcionas ou excluidos
+        --> pag_count = quantidade de resultados esperados no retorno da request 
+    """
+
+    def __init__(self,  keywords = {"include":[], "exclude":[]},
+                        parameters = {"include":{}, "exclude":{}},
+                        pag_count = 100, 
+                        all_pages = False, 
+                        more_filters = ""):
+        
+
         self.keywords = keywords
         self.parameters = parameters
         self.all_pages = all_pages
-        self.others = more
+        self.string = more_filters
         self.pag_count = 100 if all_pages else pag_count
+        self.others = {}
 
 
     def __str__(self):
-        str_keywords = " ".join(self.keywords)
-        str_parameters = ""
-        for key in self.parameters:
-            str_parameters += """ {}:\\"{}\\" """.format(key, self.parameters[key])
+        key_in = [f"""\\"{x}\\" """ for x in self.keywords["include"]]
+        key_ex = [f"""NOT \\"{x}\\" """ for x in self.keywords["exclude"]]
+        str_keywords = " ".join(key_in) + "" + " ".join(key_ex)
 
-        #search string
-        string = f"{str_keywords} {str_parameters}"
+        par_in = [f"""{x}:\\"{self.parameters["include"][x]}\\" """ for x in self.parameters["include"]]
+        par_ex = [f"""NOT {x}:\\"{self.parameters["exclude"][x]}\\" """ for x in self.parameters["exclude"]]
+        str_parameters = " ".join(par_in) + " " + " ".join(par_ex)
+
+        
         others = ""
         others += ", type: REPOSITORY"
         others += ", first: " + str(self.pag_count)
@@ -40,8 +59,11 @@ class Query_string():
         for key in self.others:
             others += """, {}:\"{}\" """.format(key, self.others[key])
 
+
+        main = f"\"{str_keywords} {str_parameters}\" {others}"
+
         query = """{
-            search(query: \"""" + string + """\" """ + others + """ ) {
+            search(query: """ + main +"""  ) {
                 repositoryCount
                 pageInfo {
                     startCursor
@@ -60,13 +82,18 @@ class Query_string():
                     }
                 }
             }"""
+
         return query
     
 
     def change_last_index(self, last_index):
         self.others["after"] = str(last_index)
 
+
 class Search():
+    """
+    Classe que gera o buscador responsável por fazer as queries
+    """
 
     global SITE 
     global ACCESS_TOKEN
@@ -81,6 +108,9 @@ class Search():
 
 
     def get_search(self, query):
+        """
+        Função que realiza a busca, inclusive buscas em sequência(all_pages)
+        """
         
         string = str(query)
         
@@ -124,7 +154,7 @@ class Search():
                 last = response["data"]["search"]["pageInfo"]["endCursor"]
                 self.get_all_pages(query, last)
 
-    
+
     def sort_repos_stars(self):
         nodes = self.response["data"]["search"]["nodes"]
         new_list = sorted(nodes, key=lambda node: node["stargazers"]["totalCount"], reverse=True)
@@ -132,25 +162,33 @@ class Search():
         self.to_file()
 
 
+    def get_firsts(self, n):
+        nodes = self.response["data"]["search"]["nodes"]
+        self.response["data"]["search"]["nodes"] = nodes[:n]
+        self.to_file()
+
+
     def results(self):
-        json_formated = json.dumps(self.response, indent=4)
+        with open("temp/result.json", "r") as f:
+            file = json.load(f)
+        json_formated = json.dumps(file, indent=4)
         print(json_formated)
 
 
     def to_file(self):
-        with open("temp/result.txt", "w") as f:
+        with open("temp/result.json", "w") as f:
             f.write(json.dumps(self.response, indent=4))
 
 
     def to_excel(self):
-        with open("temp/result.txt", "r") as f:
+        with open("temp/result.json", "r") as f:
             file = json.load(f)
 
         dic = file["data"]["search"]["nodes"]
 
-        lista = [[item["name"], item["url"]] for item in dic]
+        lista = [[item["name"], item["url"], item["stargazers"]["totalCount"]] for item in dic]
 
-        df = pd.DataFrame(lista, columns=["name", "url"])
+        df = pd.DataFrame(lista, columns=["name", "url", "star"])
 
         df.to_excel("result.xlsx", index=False)
 
@@ -158,7 +196,7 @@ class Search():
 
     def __str__(self):
         if self.in_cache:
-            with open("temp/result.txt", "r") as f:
+            with open("temp/result.self", "r") as f:
                 file = json.load(f)
             json_formated = json.dumps(self.response, indent=4)
             return json_formated
@@ -170,14 +208,19 @@ class Search():
 if __name__ == "__main__":
     s = Search()
 
-    keywords = ["paper", "experiments"]
-    parameters = {"language":"python"}
+    keywords = {"include":["paper", "experiments"], "exclude":["machine learning", "neural network", "artificial intelligence", "framework", "train"]}
+    parameters = {"include":{"language":"python"}, "exclude":{}}
 
     query = Query_string(keywords, parameters, pag_count=100, all_pages = True)
-
     s.get_search(query)
-    s.sort_repos_stars()
-    #print(str(s))
 
+    try:
+        print(s.response["data"]["search"]["repositoryCount"])
+    except:
+        pass
     pass
+
+    s.sort_repos_stars() #ordena a busca por stars
+    s.get_firsts(100) #pega apenas os n primieros da busca
+    s.to_excel() #salva a busca em um .xlsx
 
